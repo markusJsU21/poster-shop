3cc88c19690edd809f66d8575898d416cc037e75
Markus <markus.mork@iths.se>
2022-02-10 11:56:18 +0100

    Add poster.json and setup loop function on Home
    To make data handling easier.

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Posters from '../../posters.json'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    posters: [...Posters]
   },
   mutations: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    
     <h1>Welcome to Your Vue.js App</h1>
+
+    <router-link v-for="poster of posters" :key="poster.id"></router-link>
   </div>
 </template>
+
+<script>
+import ItemCard from '../components/item-card.vue'
+export default {
+  components: {ItemCard},
+  computed: {
+    posters(){
+      return this.$store.state.posters
+    }
+  }
+}
+</script>
